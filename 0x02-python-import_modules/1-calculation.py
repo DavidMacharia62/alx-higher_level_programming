b1430227e9302b419fe84d268612c5466b35a497
DavidMacharia62 <davidmacharia1@gmail.com>
2023-05-11 06:03:58 -0700

    Task 1: 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,12 +4,8 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-add_res = add(a, b)
-sub_res = sub(a, b)
-mul_res = mul(a, b)
-div_res = div(a, b)
-
-print("{:d} + {:d} = {:d}".format(a, b, add_res))
-print("{:d} - {:d} = {:d}".format(a, b, sub_res))
-print("{:d} * {:d} = {:d}".format(a, b, mul_res))
-print("{:d} / {:d} = {:d}".format(a, b, div_res))
+if __name__ == "__main__":
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
