23481e3ccec6c61b2c7fcb4678acaaf2bf317253
DavidMacharia62 <davidmacharia1@gmail.com>
2023-05-30 15:25:21 +0300

    Task 1 correct: 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 '''
-Module defining the Square class
+Module of a Square class
 '''
+
+
 class Square:
-    '''A class representing a square
+    '''A simple class that defines a square
     Attributes:
-    __size: The size of the square
-    '''
-    def __init__(self, size):
-        '''Initializer method for the Square class
-        Args:
         size: The size of the square
+    '''
+
+    def __init__(self, size):
+        '''Initialization method for the Square class
+        Args:
+            size: The size of the square
         '''
         self.__size = size
