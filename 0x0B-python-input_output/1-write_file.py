597e4704b348df0ee0adc1f568defa6190f8ac03
DavidMacharia62 <davidmacharia1@gmail.com>
2023-06-13 23:38:32 +0300

    Task 1: 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -21,5 +21,4 @@ def write_file(filename="", text=""):
 
     """
     with open(filename, "w", encoding="utf-8") as file:
-        count = file.write(text)
-        return (count)
+        return (file.write(text))
