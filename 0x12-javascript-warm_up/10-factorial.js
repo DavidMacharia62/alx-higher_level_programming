ab45c16da28da370978ab478b39f00b14ff53ad9
DavidMacharia62 <davidmacharia1@gmail.com>
2023-08-14 13:30:35 +0300

    compute and print factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// A script that computes and prints a factorial
+const argv = require('process').argv;
+
+function factorial (num) {
+  if (num === 0 || isNaN(num)) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+const num = Number(argv[2]);
+console.log(factorial(num));
