cbe8be2b199557953419c195ffe57529d3d9db4a
DavidMacharia62 <davidmacharia1@gmail.com>
2023-08-15 12:52:59 +0300

    rectangle class with w & h as args

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// A class that defines a rectangle that takes 2 args w and h
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
