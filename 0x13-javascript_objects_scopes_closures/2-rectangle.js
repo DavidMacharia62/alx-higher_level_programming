0ccb3786f63d17c579c15331eeef7c504241a5ed
DavidMacharia62 <davidmacharia1@gmail.com>
2023-08-15 12:56:39 +0300

    2 rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// A rectangle defined
+class Rectangle {
+  constructor (w, h) {
+    if ((w = parseInt(w)) > 0 && (h = parseInt(h)) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
